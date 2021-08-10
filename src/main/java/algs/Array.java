96eff23b61a010c6f6e879e33ff35cd37d7b9759
张琪 <zhangqi@smartbi.com.cn>
2021-08-10 16:04:41 +0800

    Array

---
+++ src/main/java/algs/Array.java
@@ -0,0 +1,40 @@
+package algs;
+
+public class Array<Element> {
+
+    private Element[] elements;
+    private int size;
+
+    public void add(Element e) {
+
+    }
+
+    public void add(Element e, int index) {
+
+    }
+
+    public void remove(Element e) {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public Element get(int index) {
+        return null;
+    }
+
+    public int indexOf(Element e) {
+        return -1;
+    }
+
+    public boolean contains(Element e) {
+        return false;
+    }
+
+    public void set(int index, Element e) {
+
+    }
+
+}
