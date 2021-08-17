2bc30fb5b3ffec6551c957503a7d4dbb17ca5c72
coder-qi <79892832+coder-qi@users.noreply.github.com>
2021-08-17 16:24:03 +0800

    Array

--- src/main/java/algs/LinkedList.java
+++ src/main/java/algs/LinkedList.java
@@ -1,6 +1,7 @@
 package algs;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * 双向链表的实现
@@ -29,7 +30,15 @@ public class LinkedList<Element> implements Iterable<Element> {
      * @param e 添加的元素
      */
     public void add(Element e) {
-
+        Node<Element> l = last;
+        Node<Element> newNode = new Node<>(l, e, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
     }
 
     /**
@@ -40,7 +49,30 @@ public class LinkedList<Element> implements Iterable<Element> {
      * @throws ArrayIndexOutOfBoundsException 如果索引越界（index < 0 || index >= size()）
      */
     public Element get(int index) {
-        return null;
+        checkIndex(index);
+        return getNode(index).element;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException("index: " + index + ", size: " + size);
+        }
+    }
+
+    private Node<Element> getNode(int index) {
+        if (index < size / 2) {
+            Node<Element> x = first;
+            for (int i = 0; i < index; i++) {
+                x = x.next;
+            }
+            return x;
+        } else {
+            Node<Element> x = last;
+            for (int i = size - 1; i > index; i++) {
+                x = x.prev;
+            }
+            return x;
+        }
     }
 
     /**
@@ -51,6 +83,9 @@ public class LinkedList<Element> implements Iterable<Element> {
      * @throws ArrayIndexOutOfBoundsException 如果索引越界（index < 0 || index >= size()）
      */
     public void set(int index, Element e) {
+        checkIndex(index);
+
+        getNode(index).element = e;
     }
 
     /**
@@ -60,7 +95,42 @@ public class LinkedList<Element> implements Iterable<Element> {
      * @return 如果成功移除，则返回true，否则，返回false
      */
     public boolean remove(Element e) {
+        if (e == null) {
+            for (Node<Element> x = first; x != null; x = x.next) {
+                if (x.element == null) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        } else {
+            for (Node<Element> x = first; x != null; x = x.next) {
+                if (e.equals(x.element)) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
         return true;
+    }
+
+    private void unlink(Node<Element> x) {
+        Node<Element> prev = x.prev;
+        Node<Element> next = x.next;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            x.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            x.next = null;
+        }
+        size--;
     }
 
     /**
@@ -72,8 +142,123 @@ public class LinkedList<Element> implements Iterable<Element> {
         return size;
     }
 
+    /**
+     * 清除所有的元素
+     */
+    public void clear() {
+        for (Node<Element> x = first; x != null; ) {
+            Node<Element> next = x.next;
+            x.prev = null;
+            x.next = null;
+            x.element = null;
+            x = next;
+        }
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+    /**
+     * 从左往右查找数据的索引
+     *
+     * @param e 待查找的元素
+     * @return 第一次查找到的元素的索引，如果没有查找到指定的元素，则返回-1
+     */
+    public int indexOf(Element e) {
+        int index = 0;
+        if (e == null) {
+            for (Node<Element> x = first; x != null; x = x.next) {
+                if (x.element == null) {
+                    return index;
+                }
+                index++;
+            }
+        } else {
+            for (Node<Element> x = first; x != null; x = x.next) {
+                if (e.equals(x.element)) {
+                    return index;
+                }
+                index++;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 数组是否为空
+     *
+     * @return 如果为空，返回true，否则，返回false
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 是否包含指定的元素
+     *
+     * @param e 待测试的元素
+     * @return 如果包含该元素，返回true，否则，返回false
+     */
+    public boolean contains(Element e) {
+        return indexOf(e) != -1;
+    }
+
+    @Override
+    public String toString() {
+        Iterator<Element> it = iterator();
+        if (!it.hasNext()) {
+            return "[]";
+        }
+        StringBuilder sb = new StringBuilder("[");
+        while (true) {
+            Element e = it.next();
+            sb.append(e);
+            if (!it.hasNext()) {
+                return sb.append("]").toString();
+            }
+            sb.append(",");
+        }
+    }
+
     @Override
     public Iterator<Element> iterator() {
-        return null;
+
+        return new Iterator<Element>() {
+
+            Node<Element> x = first;
+
+            @Override
+            public boolean hasNext() {
+                return x != null;
+            }
+
+            @Override
+            public Element next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Node<Element> node = x;
+                x = x.next;
+                return node.element;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        LinkedList<String> names = new LinkedList<>();
+        names.add("张三");
+        names.add("李四");
+        names.add("王五");
+        System.out.println(names);
+
+        // 存放"赵六"和"钱七"
+        names.add("赵六");
+        names.add("钱七");
+        System.out.println(names);
     }
 }
